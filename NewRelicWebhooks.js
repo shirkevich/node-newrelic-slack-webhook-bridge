c1ab54fc4cbaf44a18d8ac32b5e2be7aaa43f8b4
Chris Jamieson <jamiesoncj@gmail.com>
2015-01-06 14:00:22 +0000

    tidied up the slack message

--- NewRelicWebhooks.js
+++ NewRelicWebhooks.js
@@ -3,60 +3,19 @@ var Slack = require('./slack');
 
 var slack = new Slack(config.slack.hook_url);
 
-// function monitoringEvent(body){
-// 	var msg = '[' + body.application_name + '][' + body.severity +
-// 		'] <' + body.alert_url + '|' + body.message +
-// 		'> -- ' + body.long_description;
-// 	slack.send({
-// 		text: msg,
-// 		channel: config.slack.room,
-// 		username: config.slack.sender,
-// 		icon_url: 'http://i.imgur.com/ePrknxm.png'
-// 	}, function(err, resp){
-// 		if (err){
-// 			console.error('ERROR:\n', err);
-// 		}
-// 	});
-// }
-
-// function deployEvent(body){
-// 	var msg = '[' + body.application_name + '][' + body.created_at +
-// 		'] <' + body.deployment_url + '|' + body.deployed_by +
-// 		'> -- ' + body.description;
-// 	slack.send({
-// 		text: msg,
-// 		channel: config.slack.room,
-// 		username: config.slack.sender,
-// 		icon_url: 'http://i.imgur.com/ePrknxm.png'
-// 	}, function(err, resp){
-// 		if (err){
-// 			console.error('ERROR:\n', err);
-// 		}
-// 	});
-// }
-
 function prepareSlackMessage(body){
 	if(typeof body.alert != 'undefined'){
-		console.log('received an ALERT from New Relic. Contents: ');
 		var alert = JSON.parse(body.alert);
-		console.log(alert);
-
-		var msg = '[' + alert.application_name + '][' + alert.severity +
-		'] <' + alert.alert_url + '|' + alert.message +
-		'> -- ' + alert.long_description;
-
+		var msg = 'Alert for application: ' + alert.application_name + ' (severity: ' + alert.severity +
+		'): <' + alert.alert_url + '|' + alert.message +
+		'> (' + alert.long_description + ')';
 		return msg;
 	}
 
 	if(typeof body.deployment != 'undefined'){
-		console.log('received a DEPLOYMENT from New Relic. Contents: ');
 		var deployment = JSON.parse(body.deployment);
-		console.log(deployment);
-
-		var msg = '[' + deployment.application_name + '][' + deployment.created_at +
-		'] <' + deployment.deployment_url + '|' + deployment.deployed_by +
-		'> -- ' + deployment.description;
-
+		var msg = 'Deployment notification for ' + deployment.application_name + ': <' + deployment.deployment_url + '|' + deployment.description +
+		'> (deployed by ' + deployment.deployed_by + ')';
 		return msg;
 	}
 }
